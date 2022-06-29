7d8c8128a5ceef3f6fe329ac847201276b80cd5b
Polina <polstaver@yandex.by>
2022-06-29 14:31:47 +0300

    сохранение семинара

--- Program.cs
+++ Program.cs
@@ -1,1 +1,35 @@
-﻿
+﻿// Напишите программу, которая перевернёт одномерный массив (последний элемент будет на первом месте, а первый - на последнем и т.д.)
+
+
+// 2. Напишите программу, которая принимает на вход три числа и проверяет, может ли существовать треугольник с сторонами такой длины.
+// Теорема о неравенстве треугольника: каждая сторона треугольника меньше суммы двух других сторон.
+
+// bool NewNum(double x, double y, double z)
+// {
+//     if (x < y + z && y < x + z && z < x + y)
+//     {
+//         return true;
+//     }
+//     else
+//     {
+//         return false;
+//     }
+// }
+
+// double A = new Random().NextDouble();
+// Console.WriteLine("{0:f5}", A);
+
+// double B = new Random().NextDouble();
+// Console.WriteLine("{0:f5}", B);
+
+// double C = new Random().NextDouble();
+// Console.WriteLine("{0:f5}", C);
+
+// if (NewNum(A, B, C))
+// {
+//     Console.WriteLine("Треугольник существует");
+// }
+// else
+// {
+//     Console.WriteLine("Треугольник не существует");
+// }
