ae2937d19ae3f6dd6b6db27cf2193f52d1a8eee3
Polina <polstaver@yandex.by>
2022-06-29 21:45:55 +0300

    сохранение ДЗ первая задача

--- Program.cs
+++ Program.cs
@@ -33,3 +33,45 @@
 // {
 //     Console.WriteLine("Треугольник не существует");
 // }
+
+// ДОМАШНЕЕ ЗАДАНИЕ
+// 1.Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// int[] CreatArray(int size)
+// {
+//     int[] array = new int [size];
+//     for(int i = 0; i<size; i++)
+//     {
+//         Console.Write("Ввод " + i + " Элемент: ");
+//         array[i] = Convert.ToInt32(Console.ReadLine());
+//     }
+//     return array;
+// }
+// void ShowArray(int[] array) 
+// {
+//     for(int i = 0; i<array.Length; i++)
+//     {
+//         Console.Write(array[i] + " ");
+//     }
+// }
+
+// void Number(int[] array)
+// {
+//     int count=0;
+//     for(int i = 0; i<array.Length; i++)
+//     {
+//         if(array[i]>0)
+//         {
+//          count=count+1;
+//         }
+//     }
+// Console.WriteLine(" -> " + count);
+// }
+
+// int[] myArray = CreatArray(6);
+// ShowArray(myArray);
+// Number(myArray);
+
+// 2. Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+
+// 3.  Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями
+//      y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
