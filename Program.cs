97ec478d365771bce0a57aa9c13369645cb6bf9d
Polina <polstaver@yandex.by>
2022-07-01 17:40:07 +0300

    сохранение ДЗ

--- Program.cs
+++ Program.cs
@@ -1,5 +1,54 @@
 ﻿// Напишите программу, которая перевернёт одномерный массив (последний элемент будет на первом месте, а первый - на последнем и т.д.)
+// перевернёт одномерный массив
 
+// int[] RandomArray(int size)
+// {
+//     int[] array = new int[size];
+//     for (int i = 0; i < size; i++)
+//     {
+//         array[i] = new Random().Next(100, 1000);
+//     }
+//     return array;
+// }
+
+// void ShowArray(int[] array)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         Console.Write(array[i] + " ");
+//     }
+//     Console.WriteLine();
+// }
+
+// void SwapArray(int[] array)
+// {
+//     int buf = 0;
+//     for (int i = 0; i < array.Length / 2; i++)
+//     {
+//         buf = array[i];
+//         array[i] = array[array.Length - i - 1];
+//         array[array.Length - i - 1] = buf;
+//     }
+//     ShowArray(array);
+// }
+
+// void SwapArray1(int[] array)
+// {
+//     int[] arr = new int[array.Length];
+//     for (int i = 0, j = array.Length - 1; i < array.Length; i++, j--)
+//     {
+//         arr[i] = array[j];
+//     }
+//     ShowArray(arr);
+// }
+
+// Console.Write("Введите размер массива: ");
+// int size = Convert.ToInt32(Console.ReadLine());
+// int[] arr = RandomArray(size);
+// ShowArray(arr);
+// Console.WriteLine();
+// SwapArray(arr);
+// SwapArray1(arr);
 
 // 2. Напишите программу, которая принимает на вход три числа и проверяет, может ли существовать треугольник с сторонами такой длины.
 // Теорема о неравенстве треугольника: каждая сторона треугольника меньше суммы двух других сторон.
@@ -71,7 +120,28 @@
 // ShowArray(myArray);
 // Number(myArray);
 
-// 2. Напишите программу, которая будет преобразовывать десятичное число в двоичное.
 
-// 3.  Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями
-//      y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// 2. Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+// void Number(int num)
+// {
+//     int x = 0;
+//     int i = 0;
+//     int[] Code = new int[10];
+
+//     while (num >= 1)
+//     {
+//         x = num%2;
+//         Code[i] = x;
+//         i++;
+//         num = num/2;
+//         Console.Write(x);
+//     }
+//     Console.WriteLine();
+//     for (i = (Code.Length - 1); i >= 0; i--)
+//     {
+//         Console.Write(Code[i]);
+//     }
+// }
+// Console.WriteLine("Введите число для конвертации в двоичный код: ");
+// int N = Convert.ToInt32(Console.ReadLine());
+// Number(N);
